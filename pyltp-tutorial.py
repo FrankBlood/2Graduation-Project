6abbe73f80c528c372ba2a3c069fff97f8e95a70
FrankBlood <1062947208@qq.com>
2016-05-11 11:15:36 +0800

    pyltp-tutorial can't load models. I don't know why.

--- pyltp-tutorial.py
+++ pyltp-tutorial.py
@@ -2,3 +2,10 @@
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
+
+from pyltp import Segmentor
+segmentor = Segmentor()
+segmentor.load('E:\\Python\\pyltp\\ltp_data\\cws.model')
+words = segmentor.segment('中华人民共和国成立了。我今天高兴地去和志伟约午饭。')
+print '\t'.join(words)
+segmentor.release()

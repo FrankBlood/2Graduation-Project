ddd9159a7cf576d4e096daa69137b70834f0872e
FrankBlood <1062947208@qq.com>
2016-05-12 08:59:23 +0800

    pyltp_tutorial (still can't work, I don't know why.)

--- pyltp-tutorial.py
+++ pyltp-tutorial.py
@@ -4,8 +4,10 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 from pyltp import Segmentor
+print 'import pyltp succefully'
 segmentor = Segmentor()
-segmentor.load('E:\\Python\\pyltp\\ltp_data\\cws.model')
+segmentor.load('E:/Python/pyltp/ltp_data/cws.model')
+print 'load model.'
 words = segmentor.segment('中华人民共和国成立了。我今天高兴地去和志伟约午饭。')
 print '\t'.join(words)
 segmentor.release()
